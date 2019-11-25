ecfe17a6e8ce1754f05fc9f0284a6ffdfeb7693e
Petr <chalupa.petr93@gmail.com>
2019-11-25 21:23:42 +0100

    1.0

--- src/data/data.js
+++
@@ -1,8 +0,0 @@
-var criteriaList = [
-	{ criteria: 'Nadpis', mark: '', weight: 1 },
-	{ criteria: 'Obsah', mark: '', weight: 1 },
-	{ criteria: 'Grafická úprava', mark: '', weight: 1 },
-	{ criteria: 'Projev', mark: '', weight: 1 },
-	{ criteria: 'Zdroje', mark: '', weight: 1 },
-	{ criteria: 'Čas', mark: '', weight: 1 }
-];
