0ba5066841b55c7f1fc32b6f390cd7f6578c19c2
Petr <chalupa.petr93@gmail.com>
2019-11-25 20:19:16 +0100

    Úprava na HashRouter
    Namísto BrowserRouteru sjem nasadil hash router kvůli deploy do subfolderu.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import Student from './components/Student/Student';
 import Teacher from './components/Teacher/Teacher';
 import Notfound from './components/notfound';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 
 class App extends React.Component {
 	render() {
 		return (
-			<Router>
+			<HashRouter>
 				<Switch>
 					<Route exact path="/" component={Student} />
 					<Route path="/teacher" component={Teacher} />
 					<Route component={Notfound} />
 				</Switch>
-			</Router>
+			</HashRouter>
 		);
 	}
 }
